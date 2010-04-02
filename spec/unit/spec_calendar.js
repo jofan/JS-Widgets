da73836c71e4e834fcf96367a9024b4a1447c07f
Stefan Lidén <stefan.liden@gmail.com>
2010-04-02 15:02:43 +0200

    Updates to calendar spec

--- spec/unit/spec_calendar.js
+++ spec/unit/spec_calendar.js
@@ -21,18 +21,39 @@ describe 'App.widget.Calendar'
       Cal.today.day.should.equal thisDay
       Cal.today.date.should.equal thisDate
     end
+    
+    it 'should have a property "calendar" that refers to div.calendar object'
+    	Cal.calendar.should.have_class 'calendar'
+    end
   end
   
   describe '.isLeapYear(year)'
-    it 'should return true if year is 2008'
+    it 'should return true if year is 2008, and false if year is 2010'
       Cal.isLeapYear(2008).should.be true
       Cal.isLeapYear(2010).should.be false
     end
   end
   
+  describe '.setDateInfo(year, month, selectedDay)'
+  	it 'should update the calendar\'s date info'
+  		Cal.setDateInfo(2008, 3)
+  		Cal.year.should.equal 2008
+  		Cal.month.should.equal 3
+  	end
+  end
+  
+  describe '.update()'
+  	it 'should update table with set year (2009) and month (2)'
+  		Cal.setDateInfo(2009, 2)
+  		Cal.update()
+  		Cal.calendar.find('caption').text().should.match 'March 2009'
+  	end
+  end
+  
   describe '.show()'
     it 'should display calendar'
       Cal.show()
+      Cal.calendar.should.be_visible
     end
   end
   
@@ -40,6 +61,7 @@ describe 'App.widget.Calendar'
     it 'should hide the calendar'
       Cal.show()
       Cal.hide()
+      Cal.calendar.should.be_hidden
     end
   end
 end
